d845fa46b19c2ef4278fdd750e39b9c0e8ab3bbe
Qin Ying Chen <qinyingchen@qycs-MBP.fios-router.home>
2022-12-15 04:37:30 -0500

    add page loader

--- movies.js
+++ movies.js
@@ -1,3 +1,14 @@
+// page loader logic
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector(".center").style.visibility = "visible";
+    } else {
+        document.querySelector(".center").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
+
 window.onload = function(){
     // get the modal
     let modal = document.getElementById("myModal");
