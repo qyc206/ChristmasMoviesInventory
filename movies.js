fd225a09a65c7927ec42949ed7ea01a71c7cd94f
Qin Ying Chen <qinyingchen@qycs-MBP.fios-router.home>
2022-12-15 03:37:46 -0500

    added content

--- movies.js
+++ movies.js
@@ -1,18 +1,69 @@
 window.onload = function(){
     // get the modal
     let modal = document.getElementById("myModal");
-    // get content
-    let modalContent = document.querySelector(".modal-content");
+    // get movie content template
+    let modalContentMovie = document.querySelector("#modalMovie");
+    // get activity content template
+    let modalContentActivity = document.querySelector("#modalActivity");
+    // get message template
+    let modalContentMessage = document.querySelector("#modalMessage");
     // get all the boxes
     let boxes = document.getElementsByClassName("box");
-
-    console.log(modalContent);
 
     // loop through all boxes
     for (let i = 0; i < boxes.length; i++){
         // attach click listener for each box
         boxes[i].onclick = function(event){
+            let activity = boxes[i].getElementsByClassName("activity")[0];
+
             modal.style.display = "block";
+            if (activity != undefined){ // if there is an activity hidden
+                if (activity.id == "movie"){  // box contains a movie 
+                    modalContentMovie.style.display = "block";
+
+                    modalContentMovie.querySelector("#poster").src =
+                            activity.querySelector("#poster").src;
+                    modalContentMovie.querySelector("#poster").alt =
+                            activity.querySelector("#poster").alt;
+
+                    modalContentMovie.querySelector("#trailer").src =
+                            activity.querySelector("#trailer").src;
+                    modalContentMovie.querySelector("#trailer").alt =
+                            activity.querySelector("#trailer").alt;
+
+                    modalContentMovie.querySelector("#title").textContent = 
+                            activity.querySelector("#title").textContent;
+
+                    modalContentMovie.querySelector("#rate").textContent = 
+                            activity.querySelector("#rate").textContent;
+
+                    modalContentMovie.querySelector("#year").textContent = 
+                            activity.querySelector("#year").textContent;
+
+                    modalContentMovie.querySelector("#genre").textContent = 
+                            activity.querySelector("#genre").textContent;
+
+                    modalContentMovie.querySelector("#description").textContent = 
+                            activity.querySelector("#description").textContent;
+
+                } else {   // box contains an activity
+                    modalContentActivity.style.display = "block";
+
+                    modalContentActivity.querySelector("#activityImage").src =
+                            activity.querySelector("#activityImage").src;
+                    modalContentActivity.querySelector("#activityImage").alt =
+                            activity.querySelector("#activityImage").alt;
+
+                    modalContentActivity.querySelector("#breakHeading2").textContent = 
+                            activity.querySelector("#breakHeading2").textContent;
+
+                    modalContentActivity.querySelector("#breakDescription").innerHTML = 
+                            activity.querySelector("#breakDescription").innerHTML
+                }
+            } else {
+                modalContentMessage.style.display = "block";
+                modalContentMessage.innerHTML = "<h1>UNDER CONSTRUCTION...</h1>";
+            }
         }
     }
 
@@ -22,12 +73,18 @@ window.onload = function(){
     // when the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
+        modalContentActivity.style.display = "none";
+        modalContentMovie.style.display = "none";
+        modalContentMessage.style.display = "none";
     }
 
     // when the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
+            modalContentActivity.style.display = "none";
+            modalContentMovie.style.display = "none";
+            modalContentMessage.style.display = "none";
         }
     }
 }
